fa6d67c52e8d020589de8be110b500fac33bb242
Mathias Neerup <github@mneerup.dk>
2024-09-23 09:44:27 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,28 @@
 
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "mane+spoke1@tv2.dk"
+    AccountName               = "sandbox-aft-spoke1"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "mane+spoke1@tv2.dk"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
